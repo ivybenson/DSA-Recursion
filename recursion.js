f7762eca851acd778e109cd4d49c3c7a687c6c73
Ivy Benson <irosebenson@gmail.com>
2021-01-05 17:09:47 -0600

    additional function

--- recursion.js
+++ recursion.js
@@ -1,23 +1,36 @@
-const sumOf = function (list) {
-  // Base case
-  if (list.length === 1) {
-    return list[0];
-  }
-  // General case
-  return list[0] + sumOf(list.slice(1));
-};
+// const sumOf = function (list) {
+//   // Base case
+//   if (list.length === 1) {
+//     return list[0];
+//   }
+//   // General case
+//   return list[0] + sumOf(list.slice(1));
+// };
 
-let lst = [2, 4, 6, 8, 10];
-console.log(sumOf(lst));
+// let lst = [2, 4, 6, 8, 10];
+// console.log(sumOf(lst));
 
-function countSheep(sheepNum) {
-  if (sheepNum === 0) {
-    return "All sheep have jumped";
-  } else {
-    console.log(`${sheepNum} have jumped over the fence`);
-    totalSheep = sheepNum - 1;
-    countSheep(totalSheep);
+// function countSheep(sheepNum) {
+//   if (sheepNum === 0) {
+//     return "All sheep have jumped";
+//   } else {
+//     console.log(`${sheepNum} have jumped over the fence`);
+//     totalSheep = sheepNum - 1;
+//     countSheep(totalSheep);
+//   }
+// }
+
+// countSheep(3);
+
+function powerCalculator(a, b) {
+  const c = a ** b;
+  if (b > 0) {
+    return c;
+  } else if (b <= 0) {
+    return "exponent should be >= 0";
   }
 }
 
-countSheep(3);
+console.log(powerCalculator(10, -2));
+
+function reverseString() {}
